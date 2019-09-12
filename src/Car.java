dd992fbbfe97846b1b296c5be1bc5f77f8000aeb
alwin <alwin.george@my.jcu.edu.au>
2019-09-12 22:44:51 +1000

    Created car class

---
+++ src/Car.java
@@ -0,0 +1,23 @@
+public class Car {
+    private float carlength;
+
+    public Car(float carlength) {
+        this.carlength = carlength;
+    }
+
+    public void moveCar() {
+
+    }
+
+    public void generateCar() {
+
+    }
+
+    public float getCarlength() {
+        return carlength;
+    }
+
+    public void setCarlength(float carlength) {
+        this.carlength = carlength;
+    }
+}
