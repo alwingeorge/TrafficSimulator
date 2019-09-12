22ea3572de7834b2f73fefc3c2cfaf0ff16ccd1f
alwin <alwin.george@my.jcu.edu.au>
2019-09-12 22:41:57 +1000

    Initial commit

---
+++ src/Bike.java
@@ -0,0 +1,14 @@
+public class Bike extends Car {
+
+    public Bike(float carlength) {
+        super(carlength);
+    }
+
+    public void moveBike() {
+
+    }
+
+    public void generateBike() {
+
+    }
+}
