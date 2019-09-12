2f6632beb146475a3c3f90664c997794bf3260d1
alwin <alwin.george@my.jcu.edu.au>
2019-09-13 06:40:17 +1000

    Implemented TrafficLight system

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -4,3 +4,38 @@ class TrafficLight {
     private String signal;
     private int duration;
 
+    TrafficLight(String signal) {
+        this.signal = signal;
+    }
+
+    void simulateTrafficLight(int duration) {
+        try {
+            Random random = new Random();
+            int num = random.nextInt(2);
+            if (num == 1) {
+                this.signal = "G";
+            } else {
+                this.signal = "R";
+            }
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    String getSignal() {
+        return signal;
+    }
+
+    void setSignal(String signal) {
+        this.signal = signal;
+    }
+
+    int getDuration() {
+        return duration;
+    }
+
+    void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
