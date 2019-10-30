08d0d37ce29f839109915a4cabe9ec7a8be22291
alwin <alwin.george@my.jcu.edu.au>
2019-10-30 12:35:02 +1000

    Established the size and color of the bus

--- src/Bus.java
+++ src/Bus.java
@@ -1,5 +1,20 @@
-//public class Bus extends Car {
-//    public Bus(float carlength) {
-//        super(carlength);
-//    }
-//}
+import java.awt.Color;
+
+public class Bus extends Vehicle {
+
+    /**
+     * Constructor.
+     *
+     * @param x
+     * @param y
+     * @param width
+     * @param height
+     * @param roadNumber
+     */
+    public Bus(int x, int y, int width, int height, int roadNumber) {
+
+        super(x, y, width, height, roadNumber, Color.BLUE);
+
+    }
+
+}
