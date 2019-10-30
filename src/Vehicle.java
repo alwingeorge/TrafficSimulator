0bacf7cb1bb7806cc1adfc657b4bc1dfa8092876
alwin <alwin.george@my.jcu.edu.au>
2019-10-30 11:32:24 +1000

    Implemented getter and setter methods of all attributes in vehicle Implemented a method to update the vehicle position in every cycle.

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -32,4 +32,144 @@ public class Vehicle {
         this.velocity = 1;
 
     }
+
+    /**
+     * @return x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Set new values of x
+     *
+     * @param x
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Set new values of y
+     *
+     * @param y
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * @return width
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * Set new values of width
+     *
+     * @param width
+     */
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    /**
+     * @return height
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Set new values of height
+     *
+     * @param height
+     */
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    /**
+     * @return velocity
+     */
+    public int getVelocity() {
+        return velocity;
+    }
+
+    /**
+     * Set new values of velocity
+     *
+     * @param velocity
+     */
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+
+    /**
+     * @return color
+     */
+    public Color getColor() {
+        return color;
+    }
+
+    /**
+     * Set new values of color
+     *
+     * @param color
+     */
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    /**
+     * @return roadNumber
+     */
+    public int getRoadNumber() {
+        return roadNumber;
+    }
+
+    /**
+     * Set new values of roadNumber
+     *
+     * @param roadNumber
+     */
+    public void setRoadNumber(int roadNumber) {
+        this.roadNumber = roadNumber;
+    }
+
+    /**
+     * Updating the location of the
+     * vehicle according to the road.
+     */
+    public void updateVehicle() {
+
+        // updating the location of car
+        // according to the car.
+        switch(roadNumber) {
+
+            case 1:
+                y += velocity;
+                break;
+            case 2:
+                x -= velocity;
+                break;
+            case 3:
+                y -= velocity;
+                break;
+            case 4:
+                x += velocity;
+                break;
+
+        }
+
+    }
+
 }
