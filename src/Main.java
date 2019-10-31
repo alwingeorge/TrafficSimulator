91635110f8f000dcb5d3e7f3ea290d0ae9090fa5
alwin <alwin.george@my.jcu.edu.au>
2019-10-31 13:47:20 +1000

    Established the Main class so it extends the JFrame class to create GUI

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,55 @@
-public class Main {
-    public static void main(String[] args) {
-        Road road = new Road();
-        int i = 0;
-        while (i <= 20) {
-            road.getTrafficLight().simulateTrafficLight(1000);
-            if (road.getTrafficLight().getSignal().equals("R")) {
-                System.out.println("Car Position : " + road.getCar().carPosition + "\t" + "Traffic Light : " + road.getTrafficLight().getSignal());
-            } else if (road.getTrafficLight().getSignal().equals("G")) {
-                System.out.println("Car Position : " + road.getCar().carPosition + "\t" + "Traffic Light : " + road.getTrafficLight().getSignal());
-                road.getCar().moveCarForward();
-            }
-            i++;
-        }
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * @version 1.0
+ *
+ * To create a frame in it.
+ */
+public class Main extends JFrame {
+
+    // Attributes..
+    private static final long serialVersionUID = 1L;
+    // centered panel of frame
+    private JPanel centeredPanel;
+    // graphics panel
+    private SimulationPanel simulation;
+
+    /**
+     * Create the frame.
+     */
+    public Main() {
+
+        // Creating a JFrame.
+        setTitle("Car Simulation");
+        // closing the program on exiting.
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // setting size and location of frame.
+        setBounds(100, 100, 515, 535);
+        // creating a centered panel to add
+        // components in it.
+        centeredPanel = new JPanel();
+        centeredPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(centeredPanel);
+        centeredPanel.setLayout(null);
+
+        simulation = new SimulationPanel();
+        centeredPanel.add(simulation);
+
     }
+
+    /**
+     * Main method to start the program.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Main frame = new Main();
+        frame.setVisible(true);
+
+    }
+
 }
